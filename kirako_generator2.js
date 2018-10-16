649b65744c5d98a42dcda2dee74d0c0f266a3527
Dancsa89 <dancsa89@gmail.com>
2018-10-16 11:53:47 +0200

    generator_nem_fuggveny

--- kirako_generator2.js
+++ kirako_generator2.js
@@ -1,12 +1,18 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+let possibleNumbers = [];
 
-let gameAreaMatrix = [[]];
-	
-for(let y = 0; y <= 2; y++) {
-	for(let x = 0; x <= 2; x++) {
-		let pickedIndex = Math.floor(Math.random(numbers.length));
-		gameAreaMatrix[y][x] = numbers[pickedIndex];
-		numbers.pop(pickedIndex);
+for(let i = 0; i < 9; i++) {
+	possibleNumbers[i] = i;
+}
+
+let pickedNumber;
+let gameArea = [];
+
+for(let y = 0; y < 3; y++) {
+	gameArea[y] = [];
+	for(let x = 0; x <= 2; x++){
+		pickedNumber = Math.floor(Math.random() * possibleNumbers.length)
+		gameArea[y][x] = possibleNumbers[pickedNumber];
+		possibleNumbers.splice(pickedNumber, 1)
 	}
 }
-console.log(gameAreaMatrix);
+console.log(gameArea);
