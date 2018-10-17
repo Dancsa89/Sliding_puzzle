b244b87300f4649dd7372db4f4a172f42798f04c
Dancsa89 <dancsa89@gmail.com>
2018-10-17 15:49:57 +0200

    mindenfele

--- stepper.js
+++ stepper.js
@@ -26,24 +26,48 @@ let track = tablePackage.table(gameAreaMatrix);
 
 console.log(track);
 
-const findNulla = () => {
-  for (let j = 0; j <= gameAreaMatrix.length; j++) {
-    for (let k = 0; k <= gameAreaMatrix[j].length; k++) {
+const stepping = () => {
+  for (let j = 0; j < gameAreaMatrix.length; j++) {
+    for (let k = 0; k < gameAreaMatrix[j].length; k++) {
       let actualElement = gameAreaMatrix[j][k];
       if (actualElement === 0) {
-        let order = readlineSync.question('Merre menjünk?');
+        let order = readlineSync.question('What is the order Master?');
         switch (order) {
-          case 'jobbra':
-            let temp = gameAreaMatrix[j][k + 1];
+          case 'right':
+            let tempRight = gameAreaMatrix[j][k + 1];
             gameAreaMatrix[j][k + 1] = gameAreaMatrix[j][k];
-            gameAreaMatrix[j][k] = temp;
-            let newGameAreaMatrix = gameAreaMatrix;
-            let track2 = tablePackage.table(newGameAreaMatrix);
-            console.log(track2);
+            gameAreaMatrix[j][k] = tempRight;
+            let gameAreaMatrixRight = gameAreaMatrix;
+            let trackRight = tablePackage.table(gameAreaMatrixRight);
+            console.log(trackRight);
+            break;
+          case 'left':
+            let tempLeft = gameAreaMatrix[j][k - 1];
+            gameAreaMatrix[j][k - 1] = gameAreaMatrix[j][k];
+            gameAreaMatrix[j][k] = tempLeft;
+            let gameAreaMatrixLeft = gameAreaMatrix;
+            let trackLeft = tablePackage.table(gameAreaMatrixLeft);
+            console.log(trackLeft);
+            break;
+          case 'down':
+            let tempDown = gameAreaMatrix[j + 1][k];
+            gameAreaMatrix[j + 1][k] = gameAreaMatrix[j][k];
+            gameAreaMatrix[j][k] = tempDown;
+            let gameAreaMatrixDown = gameAreaMatrix;
+            let trackDown = tablePackage.table(gameAreaMatrixDown);
+            console.log(trackDown);
+            break;
+          case 'up':
+            let tempUp = gameAreaMatrix[j - 1][k];
+            gameAreaMatrix[j - 1][k] = gameAreaMatrix[j][k];
+            gameAreaMatrix[j][k] = tempUp;
+            let gameAreaMatrixUp = gameAreaMatrix;
+            let trackUp = tablePackage.table(gameAreaMatrixUp);
+            console.log(trackUp);
         }
       }
     }
   }
 }
 
-findNulla();
+stepping();
