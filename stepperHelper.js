a9231bccb566294a0df918fa66ff4ef72de7b07b
rartaat <rartaat@gmail.com>
2018-10-18 19:41:07 +0200

    refakt try

--- stepperHelper.js
+++ stepperHelper.js
@@ -1,40 +1,7 @@
 const tablePackage = require('table');
-const readlineSync = require('readline-sync');
-const stepHelp = require('./stepperHelper.js');
 
-let gameAreaMatrix = [];
-let pickedNumber;
-let possibleNumbers = [];
 let row = 0;
 let coll = 0;
-
-const matrix = () => {
-  for (let i = 0; i < 9; i++) {
-    possibleNumbers[i] = i;
-    if (possibleNumbers[i] === 0) {
-      possibleNumbers[i] = ' ';
-    }
-  }
-};
-
-const generator = () => {
-  matrix();
-  for (let y = 0; y <= 2; y++) {
-    gameAreaMatrix[y] = [];
-    for (let x = 0; x <= 2; x++) {
-      pickedNumber = Math.floor(Math.random() * (possibleNumbers.length));
-      gameAreaMatrix[y][x] = possibleNumbers[pickedNumber];
-      possibleNumbers.splice(pickedNumber, 1);
-    }
-  }
-  let track = tablePackage.table(gameAreaMatrix);
-  console.log(track);
-};
-
-const gameArea = () => {
-  matrix();
-  generator();
-};
 
 const clearLeft = () => {
   let gameAreaMatrixRight = gameAreaMatrix;
@@ -112,33 +79,9 @@ const moveDown = () => {
   }
 };
 
-gameArea();
-
-const stepping = () => {
-  for (row = 0; row < gameAreaMatrix.length; row++) {
-    for (coll = 0; coll < gameAreaMatrix[row].length; coll++) {
-      let actualElement = gameAreaMatrix[row][coll];
-      if (actualElement === ' ') {
-        let order = readlineSync.question('What is the order Master? ');
-        switch (order) {
-          case 'a':
-            moveLeft();
-            break;
-          case 'd':
-            moveRight();
-            break;
-          case 'w':
-            moveUp();
-            break;
-          case 's':
-            moveDown();
-            break;
-        }
-      }
-    }
-  }
+module.exports = {
+  moveDown,
+  moveLeft,
+  moveRight,
+  moveUp
 };
-
-while (true) {
-  stepping();
-}
